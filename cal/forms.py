bf177ae47fff3986964d3e95138768a8b95096e9
Antonio Thornton <antoniothornton01@gmail.com>
2022-10-20 10:47:03 -0500

    Updated

--- cal/forms.py
+++ cal/forms.py
@@ -4,7 +4,6 @@ from cal.models import Event
 class EventForm(ModelForm):
   class Meta:
     model = Event
-    # datetime-local is a HTML5 input type, format to make date time show on fields
     widgets = {
       'start_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
       'end_time': DateInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
@@ -13,6 +12,5 @@ class EventForm(ModelForm):
 
   def __init__(self, *args, **kwargs):
     super(EventForm, self).__init__(*args, **kwargs)
-    # input_formats parses HTML5 datetime-local input to datetime field
     self.fields['start_time'].input_formats = ('%Y-%m-%dT%H:%M',)
     self.fields['end_time'].input_formats = ('%Y-%m-%dT%H:%M',)
